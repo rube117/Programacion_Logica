23d674ccc7cf2e806ba798c45c1d9a7881c403bc
rube117 <rube11_7@hotmail.com>
2019-02-19 10:05:58 -0600

    Son ejemplos de programación

---
+++ Ejemplos/Ejemplo 1/Frutas.pl
@@ -0,0 +1,20 @@
+% Programa de Frutas
+% *** Vamos a ver el manejo de variables ***
+% Autor: Rubén Verduzco López
+
+limpiar :- write('\033[2J'). % Limpia la pantalla
+
+es_fruta(uva).
+es_fruta(sandia).
+es_fruta(melon).
+es_fruta(melocoton).
+es_fruta(membrillo).
+
+fruta(sandia,roja,planta).
+fruta(melon,verde,arbol).
+fruta(platano,amarillo,arbol).
+fruta(platano,rojo,arbol).
+fruta(uva,verde,parra).
+fruta(manzana,roja,arbol).
+
+se_como(platano).
