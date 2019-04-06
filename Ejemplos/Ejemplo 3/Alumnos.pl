fc100b93ee35b3ad676dfeefc9a5c12ad3546ac0
rube117 <rube11_7@hotmail.com>
2019-04-06 14:35:33 -0600

    Más ejemplos

---
+++ Ejemplos/Ejemplo 3/Alumnos.pl
@@ -0,0 +1,47 @@
+/*
+
+Ejemplo 3
+Autor: Rubén Verduzco López.
+
+*/
+
+limpiar :- write('\e[2J'). % Limpia la pantalla.
+
+% Hechos
+
+% alumno/1 --> alumno(Nombre).
+alumno('Eva').
+alumno('Luís').
+alumno('José').
+alumno('Luisa').
+alumno('Raúl').
+
+% edad/2 --> edad(Nombre,Edad).
+edad('Luís',23).
+edad('Luís',23).
+edad('Esteban',10).
+edad('Pedro',23).
+edad('Pepe',44).
+
+% temperatura/2 --> temperatura(Día,CalTem).
+temperatura('Jueves','Baja').
+
+% sin_sol/1 --> sin_sol(Día).
+sin_sol('Jueves').
+
+% Reglas
+
+% par/1 --> par(Numero).
+par(Numero):-0 is Numero mod 2.
+
+% nublado/1 --> nublado(Dia).
+nublado(Dia):-sin_sol(Dia).
+
+% frio/1 --> frio(Dia).
+frio(Dia):-temperatura(Dia,'Baja').
+
+% llueve/1 --> llueve(Dia).
+llueve(Dia):-nublado(Dia),frio(Dia).
+
+tl :- limpiar,trace. % Limpia y entra a la traza.
+stl :- nodebug,limpiar.
