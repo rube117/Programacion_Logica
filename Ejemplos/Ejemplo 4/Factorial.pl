c806fdbbaa538d3e97333914629518286d019b79
rube117 <rube11_7@hotmail.com>
2019-04-09 11:34:03 -0500

    Ejemplo, recursividad.

---
+++ Ejemplos/Ejemplo 4/Factorial.pl
@@ -0,0 +1,14 @@
+/*
+ Ejemplo: Factorial
+ Autor: Rubén Verduzco López
+*/
+
+limpiar :- write('\e[2J'). % Limpia la pantalla.
+tl :- limpiar,trace. % Limpia y entra a la traza.
+stl :- nodebug,limpiar. % Sale de la traza y limpia la pantalla.
+
+% Obtener factorial
+
+% Regla de la condición final.
+factorial(0,1):-!.
+factorial(Num,Resultado):-X is Num-1,factorial(X,W),Resultado is Num*W.
